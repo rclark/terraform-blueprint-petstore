0d9fe7bf826c882cad2b146ad4ddda176a691324
Nicola Sheldrick <sheldr@gmail.com>
2023-08-09 16:28:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,6 @@ resource "random_pet" "pet" {
   length = var.pet-count
 }
 
-resource "random_pet" "pet2" {
-  length = var.pet-count
-}
-
 resource "random_string" "random_word" {
   length = var.word-length
 }
